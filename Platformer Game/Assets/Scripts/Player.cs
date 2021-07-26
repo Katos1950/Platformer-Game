8df48876c204c032799a567a152d7497ba275903
Katos1950 <87760385+Katos1950@users.noreply.github.com>
2021-07-26 14:23:50 +0530

    Animaions added to Player Movement

--- Platformer Game/Assets/Scripts/Player.cs
+++ Platformer Game/Assets/Scripts/Player.cs
@@ -8,29 +8,47 @@ public class Player : MonoBehaviour
     [SerializeField] float jumpSpeed = 100;
 
     Rigidbody2D rigidBody;
+    Animator animator;
     bool isGrounded = false;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Movement
-        if(SimpleInput.GetAxis("Horizontal") > 0f)
+        //Movement and animation
+        if(isGrounded)
         {
+            animator.SetBool("Jump", false);
+        }
+
+        if (SimpleInput.GetAxis("Horizontal") > 0f)
+        {
+            animator.SetBool("Run", true);
+            transform.localScale = new Vector2(1,1);
             transform.Translate(transform.right * speed * Time.deltaTime);
         }
+
         if (SimpleInput.GetAxis("Horizontal") < 0f)
         {
+            animator.SetBool("Run", true);
+            transform.localScale = new Vector2(-1,1);//inverting player
             transform.Translate(transform.right * -speed * Time.deltaTime);
         }
+
         if(SimpleInput.GetAxis("Vertical") > 0f && isGrounded)
         {
+            animator.SetBool("Jump", true);
             rigidBody.velocity = Vector2.up * jumpSpeed * Time.fixedDeltaTime;
-            //rigidBody.AddForce(Vector2.up * jumpSpeed * Time.deltaTime,ForceMode2D.Force);
+        }
+
+        if(SimpleInput.GetAxis("Horizontal") == 0f && SimpleInput.GetAxis("Vertical") == 0f)
+        {
+            animator.SetBool("Run", false);
         }
     }
 
